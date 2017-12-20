5cd3c9c21796a535cecd34103a86dfe759058097
Nicolas Relange <nrelange@lemoinetechnologies.com>
2017-12-20 11:10:46 +0100

    Initial version

---
+++ README.md
@@ -0,0 +1,73 @@
+# grunt-graph-easy
+
+> Use graph-easy to convert text graphs into images
+
+
+## Getting Started
+
+### Requirements
+
+* Grunt
+
+* graph-easy (on Debian: sudo apt install libgraph-easy-perl)
+
+
+### Installation
+
+Install this plugin:
+
+```shell
+$ npm install grunt-graph-easy --save-dev
+```
+
+
+Include the task in your Gruntfile:
+
+```js
+grunt.loadNpmTasks('grunt-graph-easy');
+```
+
+
+## ocamlbuild task
+
+_Run this task with the `grunt graph-easy` command._
+
+### Options
+
+#### format
+Type: `String`
+
+Output format: `ascii`, `png`
+
+Default: `ascii`
+
+
+#### grapheasy
+Type: `String array`
+
+Additional options to be passed to `graph-easy`.
+
+
+### Usage Examples
+
+```js
+graph-easy: {
+  main: {
+    files: {
+      'dist/images': 'graph/a.txt'
+    },
+    options:
+      format: 'ascii',
+      grapheasy: []
+  },
+},
+```
+
+This task supports all the file mapping format Grunt supports. Please read [Globbing patterns](http://gruntjs.com/configuring-tasks#globbing-patterns) and [Building the files object dynamically](http://gruntjs.com/configuring-tasks#building-the-files-object-dynamically) for additional details.
+
+
+##License
+
+Copyright (c) 2017 Lemoine Automation Technologies
+
+Licensed under the MIT license.
---
+++ tasks/graph-easy.js
@@ -0,0 +1,77 @@
+/*
+ * grunt-of-ocamlbuild
+ *
+ * Copyright (c) 2017 Nicolas Relange, contributors
+ * Licensed under the MIT license.
+ */
+
+module.exports = function (grunt) {
+  'use strict';
+
+  var path = require('path');
+
+  grunt.registerMultiTask('graph-easy', 'Use graph-easy to convert text graphs into images', function () {
+    var done = this.async();
+
+    var options = this.options();
+    var format = options.format;
+
+    var run = function (graphFile, destination) {
+      var src;
+      if (grunt.file.isPathAbsolute(graphFile)) {
+        src = path.basename(graphFile);
+      }
+      else {
+        src = graphFile
+      }
+      var imageFile = src.replace(/\.[^.]+$/, '.' + format);
+      var imagePath = path.join(destination, imageFile);
+
+      grunt.file.mkdir(destination);
+
+      var runDone = function (error, result, code) {
+        if (error) {
+          grunt.log.error('graph-easy error:');
+          grunt.log.writeln(result.stdout);
+          grunt.log.writeln(result.stderr);
+          grunt.fail.warn('graph-easy error');
+        }
+        else {
+          grunt.log.ok(imagePath + ' was successfully created');
+          grunt.verbose.writeln(result);
+          done();
+        }
+      }
+
+      grunt.verbose.writeln('Image file path is  ' + imagePath);
+      grunt.log.writeln('Create ' + imageFile + ' into ' + destination);
+      var grapheasy_args = options.grapheasy.concat(['--input=' + graphFile, '--output=' + imagePath, '--as', format]);
+      grunt.log.writeln('graph-easy ' + ocamlbuild_args.join(' '));
+      grunt.util.spawn({
+        cmd: 'graph-easy',
+        args: grapheasy_args
+      }, runDone);
+    }
+
+    this.files.forEach(function (f) {
+      var destDirectory = unixifyPath(f.dest);
+      var graphFiles = f.src.filter(function (filepath) { return true; })
+
+      graphFiles.forEach(function (source) {
+        var graphFile = unixifyPath(source);
+        grunt.verbose.writeln('Convert ' + graphFile + ' into ' + destDirectory + ' with format ' + format);
+        run(graphFile, destDirectory);
+      })
+    });
+
+  });
+
+
+  var unixifyPath = function (filepath) {
+    if (process.platform === 'win32') {
+      return filepath.replace(/\\/g, '/');
+    } else {
+      return filepath;
+    }
+  };
+};
